457f31553c9a73413713f0e2dd217a00edf1bfb7
Gal Tsubery <gal.tsubery@gmail.com>
2026-01-06 23:56:16 -0500

    More prominent shadow

--- app/src/main/java/com/galtagency/pointstracker/MainActivity.kt
+++ app/src/main/java/com/galtagency/pointstracker/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -34,16 +33,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.galtagency.pointstracker.ui.theme.PointsTrackerTheme
 
 
@@ -135,8 +130,8 @@ fun MainScreen(
                 fontWeight = FontWeight.Bold,
                 shadow = Shadow(
                     color = outlineColor,
-                    offset = Offset(2f, 2f),
-                    blurRadius = 4f
+                    offset = Offset(4f, 4f),
+                    blurRadius = 8f
                 )
             )
 
